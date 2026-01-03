6987e33097f249c4168b6c3368a07912ae1d5b7e
alonefact <104738034+alonefact@users.noreply.github.com>
2026-01-03 02:26:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import json, random
 
 Emotes = json.load(open("Emotes.json", encoding="utf-8"))
+
+TotalEmotes = len(Emotes)
+UniqueCreators = len(set(Emote["CreatorId"] for Emote in Emotes))
+
+print(f"Total Emotes: {TotalEmotes}")
+print(f"Unique Creators: {UniqueCreators}\n")
+print("-" * 50, "\n")
+
 RandomEmote = random.choice(Emotes)
 
 for Index, Value in RandomEmote.items():
